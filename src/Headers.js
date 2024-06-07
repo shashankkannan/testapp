259586931f27df451541c99397fdebb2cb18b45e
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-06-07 14:31:20 -0400

    Update change 2

---
+++ src/Headers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export const Headers = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          This is the header section
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};

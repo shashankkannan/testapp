f6af0d55d7e438c9c9777ba14c89d5ddc9bbcf93
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-06-08 14:43:28 -0400

    Update change 19

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { motion } from 'framer-motion';
 import { Headers } from './Components/Headers';
 import { Projects } from './Components/Projects';
 import { Id } from './Components/Id';
@@ -7,6 +8,7 @@ import { Id } from './Components/Id';
 const App = () => {
   return (
     <div className="App">
+      <motion div className='cursor'></motion>
       <Headers />
       <Id/>
       <Projects />

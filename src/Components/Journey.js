c4817e4e8da67c30ec7d25a9d5dae96f5683afaa
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-06-09 00:29:30 -0400

    Update change 24

--- src/Components/Journey.js
+++ src/Components/Journey.js
@@ -1,6 +1,6 @@
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import 'react-vertical-timeline-component/style.min.css';
-import {FaGraduationCap} from 'react-icons/fa';
+import {FaGraduationCap, FaBriefcase} from 'react-icons/fa';
 export const Journey = () => {
     
     return(
@@ -8,10 +8,17 @@ export const Journey = () => {
             <h1>Journey</h1>
             <VerticalTimeline lineColor="#000">
                 <VerticalTimelineElement className="vertical-timeline-element--education" 
-                iconStyle={{background: "#000", color:'#fff'}} icon={<FaGraduationCap/>}>
-                    
+                iconStyle={{background: "#000", color:'#fff'}} icon={<FaGraduationCap/>}
+                date="Jun 2017- Jun 2021">
+                    <h3>Computer Science & Engineering</h3>
+                    <p>Bachelor of Technology - Sastra University </p>
+                </VerticalTimelineElement>
 
-
+                <VerticalTimelineElement className="vertical-timeline-element--education" 
+                iconStyle={{background: "#000", color:'#fff'}} icon={<FaBriefcase/>}
+                date="Dec 2020- May 2021">
+                    <h3>Software Developer </h3>
+                    <p>Vrsavvy Technology </p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
